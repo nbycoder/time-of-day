cb209db5e98f7095e936e9629f9b48cd2611913a
ellis <90400844+nbycoder@users.noreply.github.com>
2021-10-02 18:27:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar() {
+var msg = document.getElementById("msg")
+var img = document.getElementById("imagem")
+var heading = document.getElementsByTagName("h1")
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    // BOM DIA!
+    img.src = 'imagens/manha.png'
+    document.body.style.backgroundColor = '#d8be88'
+
+}
+else if (hora >= 12 && hora <= 18) {
+    // BOA TARDE!
+    img.src = 'imagens/tarde.png'
+    document.body.style.backgroundColor = '#e9a76d'
+}
+else {
+    // BOA NOITE!
+    img.src = 'imagens/noite.png'
+    document.body.style.backgroundColor = '#79659f'
+}
+}
