513af0fc3a251a96da4419857e313ab480310f82
ellis <90400844+nbycoder@users.noreply.github.com>
2021-10-03 14:19:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ if (hora >= 0 && hora < 12) {
     // BOM DIA!
     img.src = 'imagens/manha.png'
     document.body.style.backgroundColor = '#d8be88'
-
 }
 else if (hora => 12 && hora <= 18) {
     // BOA TARDE!
