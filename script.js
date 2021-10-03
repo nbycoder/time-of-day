c0e6147e6770661835e36f48c986f4467d9e22cd
ellis <90400844+nbycoder@users.noreply.github.com>
2021-10-03 14:20:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var img = document.getElementById("imagem")
 var data = new Date()
 var hora = data.getHours() + ':' + data.getMinutes()
 msg.innerHTML = `It's currently ${hora}.`
-if (hora >= 0 && hora < 12) {
+if (hora >= 0 && hora <= 12) {
     // BOM DIA!
     img.src = 'imagens/manha.png'
     document.body.style.backgroundColor = '#d8be88'
 }
-else if (hora => 12 && hora <= 18) {
+else if (hora > 12 && hora <= 18) {
     // BOA TARDE!
     img.src = 'imagens/tarde.png'
     document.body.style.backgroundColor = '#e9a76d'
