f027f9dc5c28dcb9797ce20a1a1d21b30ccee2ea
ellis <90400844+nbycoder@users.noreply.github.com>
2021-10-03 15:57:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function carregar() {
-var msg = document.getElementById("msg")
-var img = document.getElementById("imagem")
-var data = new Date()
-var hora = data.getHours() + ':' + data.getMinutes()
-msg.innerHTML = `It's currently ${hora}.`
-if (hora >= 0 && hora <= 12) {
-    // BOM DIA!
-    img.src = 'imagens/manha.png'
-    document.body.style.backgroundColor = '#d8be88'
-}
-else if (hora > 12 && hora <= 18) {
-    // BOA TARDE!
-    img.src = 'imagens/tarde.png'
-    document.body.style.backgroundColor = '#e9a76d'
-}
-else {
-    // BOA NOITE!
-    img.src = 'imagens/noite.png'
-    document.body.style.backgroundColor = '#79659f'
-}
-}
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var data = new Date()
+    var time = data.getHours()
+    var clock = time + ':' + data.getMinutes()
+    msg.innerHTML = `It's currently ${clock}.`
+    if (time >= 0 && time < 12) {
+        // GOOD MORNING!
+        img.src = 'imagens/manha.png'
+        document.body.style.backgroundColor = '#d8be88'
+    
+    }
+    else if (time => 12 && time <= 18) {
+        // GOOD AFTERNOON!
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = '#e9a76d'
+    }
+    else {
+        // GOOD EVENING!
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = '#79659f'
+    }
+    }    
